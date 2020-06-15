22cea129a09165bd7b99467808f5a83ad0aeb13f
OpinionatedPotato <53105805+OpinionatedPotato@users.noreply.github.com>
2020-06-14 23:25:12 -0400

    Using stripping tool to get Google Maps Data
    Stripping tool is open source code found on GitHub: https://github.com/m-wrzr/populartimes

---
+++ gettingPopularTimesGoogleData.py
@@ -0,0 +1,31 @@
+import populartimes
+file = open("popularTimesData.txt", "w")
+
+def getTimes(location, event):
+    file.write(event + ": \n")
+    for i in range(7):
+        busytimes = location[i].get("data")
+        busyString = ""
+
+        for j in range(24):
+            if (j == 23):
+                busyString += str(busytimes[j]) + "\n"
+            else:
+                busyString += str(busytimes[j]) + " "
+
+        file.write(busyString)
+
+supermarket = populartimes.get_id("AIzaSyBeDXoRG4AHN8v_q5mhd5FZs-XxehrsXbc", "ChIJQ_Qwf9VCK4gRgiLkTszqEv0")
+supermarket = supermarket.get("populartimes")
+supermarketTimes = getTimes(supermarket, "Supermarket")
+
+bank = populartimes.get_id("AIzaSyBeDXoRG4AHN8v_q5mhd5FZs-XxehrsXbc", "ChIJUU6mgkddK4gRti44O1Lze2Q")
+bank = bank.get("populartimes")
+bankTimes = getTimes(bank, "Bank")
+
+pharmacy = populartimes.get_id("AIzaSyBeDXoRG4AHN8v_q5mhd5FZs-XxehrsXbc", "ChIJGZ-j11NDK4gRlhHS7708QXY")
+pharmacy = pharmacy.get("populartimes")
+pharmacyTimes = getTimes(pharmacy, "Pharmacy")
+
+
+
